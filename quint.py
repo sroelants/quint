e03a04a952195d8693789f9e5c9064de42436bc0
sroelants <roelants.sam@gmail.com>
2015-10-14 22:33:07 +0200

    Added quint.py file.

---
+++ quint.py
@@ -0,0 +1,11 @@
+"""
+An interface to facilitate a chain of Quantum Espresso calculations
+
+We create a modular structure in which Quantum Espresso calculations can easily
+be chained and combined as needed. This makes redundant copying of input
+parameters across calculations unnecessary, and automatically generates all the
+parsed files I want. In essence, this is a structured framework to do the many
+hacky scripts I've written over the years.
+"""
+
+
