28d6bda996ac6d37dafcc3e289c84ce6c725d134
Sam Roelants <roelants.sam@gmail.com>
2015-10-15 10:17:00 +0200

    Removed unnecessary classes

--- calculation.py
+++ calculation.py
@@ -1,12 +1,4 @@
 class Calculation(object):
     """ An abstract class all calculations inherit from. """
-""" This is a comment!"""
 class ScfCalculation(Calculation):
     """ An SCF calculation. """
-
-class RelaxCalculation(Calculation):
-    """ A relaxation calculation. """
-
-class VcCalculation(Calculation):
-    """ A variable cell relaxation calculation."""
-
