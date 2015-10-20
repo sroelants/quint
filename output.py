505c723d69ccab3b0d54e9ce715714b15a2945fb
Sam Roelants <roelants.sam@gmail.com>
2015-10-21 00:14:28 +0200

    Finished (mostly) the ParitiesOutput class

--- output.py
+++ output.py
@@ -1,13 +1,15 @@
 import numpy as np
 import re
+
+
 class PwxOutput:
     def __init__(self, out):
         self.outfile = out
-    
+
     def total_energy(self):
         """Parse output file to get total energy
 
-        Because this method should also work for relaxation 
+        Because this method should also work for relaxation
         runs, in which a total energy is printed for every iteration,
         we traverse the file bottom-up, and return the first total energy we
         find.
@@ -15,13 +17,13 @@ class PwxOutput:
         with open(self.outfile) as handle:
             for line in handle.readlines().reverse():
                 words = line.split()
-                if words and words[0]=='!':
+                if words and words[0] == '!':
                     return words[4]
 
     def fermi_energy(self):
         """Parse output file to get Fermi energy
 
-        Because this method should also work for relaxation 
+        Because this method should also work for relaxation
         runs, in which the Fermi energy is printed for every iteration,
         we traverse the file bottom-up, and return the first Fermi energy we
         find.
@@ -29,12 +31,14 @@ class PwxOutput:
         with open(self.outfile) as handle:
             for line in handle.readlines().reverse():
                 words = line.split()
-                if len(words) == 6 and words[1]=='Fermi':
+                if len(words) == 6 and words[1] == 'Fermi':
                     return words[4]
+
 
 class ScfOutput(PwxOutput):
     def __init__(self, out):
         super().__init__(out)
+
 
 class RelaxOutput(PwxOutput):
     """ A relaxation calculation with variable atomic positions
@@ -45,26 +49,27 @@ class RelaxOutput(PwxOutput):
     """
     def __init__(self, out):
         super().__init__(out)
-        
+
     def relaxed_coordinates(self):
         """ Parse output file to get the relaxed coordinates
-       
+
         Split the file content at the 'Begin final coordinates' and "End final
         coordinates' part, and extract the new coordinates.
         """
         with open(self.outfile) as handle:
             content = handle.read()
         # Split content until we get the relevant section
-        before, relevant = content.split('Begin final coordinates',1)
-        cruft,relevant = relevant.split('ATOMIC_POSITIONS (crystal)',1)
-        relevant, after = relevant.split('End final coordinates',1) 
-        
+        before, relevant = content.split('Begin final coordinates', 1)
+        cruft, relevant = relevant.split('ATOMIC_POSITIONS (crystal)', 1)
+        relevant, after = relevant.split('End final coordinates', 1)
+
         # Get coordinates in a list of string numbers
         coordinates = relevant.splitlines()[0:]
         # Convert them into arrays of floats
-        coordarrays = [np.array( list(map(float, x.split()[1:]))) 
-                        for x in coordinates]
+        coordarrays = [np.array(list(map(float, x.split()[1:])))
+                       for x in coordinates]
         return coordarrays
+
 
 class VcRelaxOutput(RelaxOutput):
     """ Relaxation calculation with variable cell dimensions.
@@ -76,10 +81,10 @@ class VcRelaxOutput(RelaxOutput):
     """
     def __init__(self, out):
         super().__init__(out)
-    
+
     def relaxed_alat(self):
         """ Parse output file to get the relaxed coordinates
-       
+
         Split the file content at the 'Begin final coordinates' and "End final
         coordinates' part, and extract the new lattice constant.
 
@@ -87,15 +92,15 @@ class VcRelaxOutput(RelaxOutput):
         is again (1.0, 0.0, 0.0). This means this routine will NOT work if the
         first lattice vector is not along the x-direction!
         """
-        
+
         with open(self.outfile) as handle:
             content = handle.read()
         # Split content until we get the relevant section
-        before, relevant = content.split('Begin final coordinates',1)
-        cruft,relevant = relevant.split('CELL_PARAMETERS',1)
+        before, relevant = content.split('Begin final coordinates', 1)
+        cruft, relevant = relevant.split('CELL_PARAMETERS', 1)
         lines = relevant.splitlines()
-        
-        alat = lines[0].split()[1] # Containts trailing ')' !
+
+        alat = lines[0].split()[1]  # Containts trailing ')' !
         # Strip non-numeric characters from alat
         non_decimal = re.compile(r'[^\d.]+')
         alat = float(non_decimal.sub('', alat))
@@ -103,9 +108,13 @@ class VcRelaxOutput(RelaxOutput):
         scale = float(lines[1].split()[0])
         return alat*scale
 
+
 class BandsOutput(PwxOutput):
     def __init__(self, out, dat):
         super().__init__(out)
         self.dat = dat
 
 
+class ParitiesOutput(PwxOutput):
+    def __init__(self, outputfiles):
+        self.outfiles = outputfiles
